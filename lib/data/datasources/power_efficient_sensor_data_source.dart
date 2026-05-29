4f317d2eb48ff0e1643e448827783e99c8fda6f6
Lintang <lintangwaladi@gmail.com>
2026-05-29 21:39:01 +0700

    feat: implement PowerEfficientSensorDataSource and add flutter_secure_storage dependency

--- lib/data/datasources/power_efficient_sensor_data_source.dart
+++ lib/data/datasources/power_efficient_sensor_data_source.dart
@@ -1,16 +1,16 @@
 import 'dart:async';
-import 'package:sensors_plus/sensors_plus.dart';
-import '../../domain/entities/accelerometer_event.dart';
+import 'package:sensors_plus/sensors_plus.dart' as sensors hide AccelerometerEvent;
+import '../../domain/entities/accelerometer_event.dart' as domain;
 import '../../domain/entities/device_posture.dart';
 import 'sensor_data_source.dart';
 
 class PowerEfficientSensorDataSource implements SensorDataSource {
   @override
-  Stream<AccelerometerEvent> get accelerometerEventStream {
-    // Dynamically limit sampling interval using SensorInterval.normal (50Hz / 200ms)
+  Stream<domain.AccelerometerEvent> get accelerometerEventStream {
+    // Dynamically limit sampling interval using SensorInterval.normalInterval
     // to keep CPU wake locks and battery consumption under the 3% per hour threshold.
-    return accelerometerEvents(samplingPeriod: SensorInterval.normal)
-        .map((event) => AccelerometerEvent(x: event.x, y: event.y, z: event.z));
+    return sensors.accelerometerEventStream(samplingPeriod: sensors.SensorInterval.normalInterval)
+        .map((event) => domain.AccelerometerEvent(x: event.x, y: event.y, z: event.z));
   }
 
   @override
@@ -21,7 +21,7 @@ class PowerEfficientSensorDataSource implements SensorDataSource {
         .distinct();
   }
 
-  DevicePosture _evaluatePosture(AccelerometerEvent event) {
+  DevicePosture _evaluatePosture(domain.AccelerometerEvent event) {
     final double y = event.y.abs();
     final double z = event.z.abs();
 
